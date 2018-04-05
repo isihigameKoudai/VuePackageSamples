f3d4ec63cd6647b7c0785f290ff9cd644bbaeb1f
石亀広大 <kira5052@gmail.com>
2018-04-05 21:12:36 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="g-content">
+    <h1>{{title}}</h1>
+    <input 
+      type="text" 
+      v-model="message" 
+    />
+    <p>input message=> {{message}}</p>
+    <input 
+      type="button"
+      value="DEVELOP"
+      @click="commitModeToDev"
+    />
+    <input 
+      type="button"
+      value="PRODUCT"
+      @click="commitModeToPro"
+    />
+    <p>mode=>{{mode}}</p>
+  </div>
+</template>
+<script>
+import {mapState, mapMutations, mapActions} from 'vuex';
+
+export default {
+  data() {
+    return {
+      title: 'Welcome to Vue world',
+      message:''
+    }
+  },
+  computed: mapState(["mode"]),
+  methods: mapMutations(["commitModeToDev","commitModeToPro"])
+}
+</script>
+<style lang="scss" scoped>
+.g-content {
+  background-color: #eee;
+  .title {
+    font-size: 24px;
+    font-weight: bold;
+  }
+}
+</style>
---
+++ src/index.js
@@ -0,0 +1,15 @@
+import 'babel-polyfill';
+
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+import App from './App.vue';
+import store from './store';
+
+Vue.use(Vuex);
+
+new Vue({
+  el:'#app',
+  store,
+  render: h => h(App)
+});
